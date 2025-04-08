3a6b3cc08b2283dc86ecb39df6b931e2999fac56
Jacob <8190899+hamy710@users.noreply.github.com>
2025-04-08 10:48:46 +1000

    sample init

---
+++ modular-website/build.gradle.kts
@@ -0,0 +1,32 @@
+
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.1.20"
+    id("io.ktor.plugin") version "3.1.2"
+}
+
+group = "com.sundriedham"
+version = "0.0.1"
+
+application {
+    mainClass = "com.sundriedham.ApplicationKt"
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-auth")
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-server-auth-jwt")
+    implementation("io.ktor:ktor-server-netty")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-test-host")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
